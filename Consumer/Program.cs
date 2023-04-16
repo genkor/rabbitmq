c6054eea7909fe50f248073453abbcbd974cda8b
Gennady Korpachev <genkor@yahoo.com>
2023-04-16 07:37:15 +0300

    Create Consumer and Producer dotnet console applications

---
+++ Consumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "192.168.30.171" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+var queueName = "letterbox";
+channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message Received: '{message}' on queue '{queueName}'");
+};
+
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+Console.ReadKey();
---
+++ Producer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "192.168.30.171" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+var queueName = "letterbox";
+channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var message = "This is my first Message.";
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("", queueName, null, encodedMessage);
+
+Console.WriteLine($"Published message: '{message}' on queue '{queueName}'");
